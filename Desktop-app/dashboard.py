cda6677426ef5a87ac0294b7ef1707a8c5d334e9
El Mo'men Bellah Sayed Mohamed <53244021+momen-sayed0@users.noreply.github.com>
2025-02-12 07:55:33 +0200

    v0.1

---
+++ Desktop-app/dashboard.py
@@ -0,0 +1,41 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUiType
+
+
+FormClass, _ = loadUiType(os.path.join(os.path.dirname(__file__), 'dashboard.ui'))
+
+class Dashboard(QMainWindow, FormClass):
+    def __init__(self, role):
+        super().__init__()
+        self.setupUi(self)
+        self.role = role
+
+        self.stackedWidget.setCurrentIndex(0) 
+        self.patient_btn.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        self.Disease_btn.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+        self.report_btn.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(3))
+        self.logout_btn.clicked.connect(self.logout)
+
+        self.configure_permissions()
+
+    def configure_permissions(self):
+        if self.role == "Assistant":
+            self.Disease_btn.hide()
+
+    def logout(self):
+        self.close()
+        from main import MainApp
+        self.login_window = MainApp() 
+        self.login_window.show()
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Dashboard("Assistant")
+    window.show()
+    sys.exit(app.exec_())
